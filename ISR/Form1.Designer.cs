23f358e8de8b8c46a3c8d1a07a4311dcde4639ff
naslylu <jaquezlu90@gmail.com>
2024-02-07 05:24:44 -0400

    Agregar archivos de proyecto.

---
+++ ISR/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ISR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_SueldoBruto = new System.Windows.Forms.TextBox();
+            this.txt_AFP = new System.Windows.Forms.TextBox();
+            this.txt_SFS = new System.Windows.Forms.TextBox();
+            this.txt_SueldoNeto = new System.Windows.Forms.TextBox();
+            this.txt_ISR = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_CalcularSueldo = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SueldoBruto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AFP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 172);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SFS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 222);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SueldoNeto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(371, 222);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ISR";
+            // 
+            // txt_SueldoBruto
+            // 
+            this.txt_SueldoBruto.Location = new System.Drawing.Point(196, 75);
+            this.txt_SueldoBruto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txt_SueldoBruto.Name = "txt_SueldoBruto";
+            this.txt_SueldoBruto.Size = new System.Drawing.Size(116, 20);
+            this.txt_SueldoBruto.TabIndex = 5;
+            // 
+            // txt_AFP
+            // 
+            this.txt_AFP.Location = new System.Drawing.Point(196, 122);
+            this.txt_AFP.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txt_AFP.Name = "txt_AFP";
+            this.txt_AFP.Size = new System.Drawing.Size(116, 20);
+            this.txt_AFP.TabIndex = 6;
+            // 
+            // txt_SFS
+            // 
+            this.txt_SFS.Location = new System.Drawing.Point(196, 169);
+            this.txt_SFS.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txt_SFS.Name = "txt_SFS";
+            this.txt_SFS.Size = new System.Drawing.Size(116, 20);
+            this.txt_SFS.TabIndex = 7;
+            // 
+            // txt_SueldoNeto
+            // 
+            this.txt_SueldoNeto.Location = new System.Drawing.Point(196, 219);
+            this.txt_SueldoNeto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txt_SueldoNeto.Name = "txt_SueldoNeto";
+            this.txt_SueldoNeto.Size = new System.Drawing.Size(116, 20);
+            this.txt_SueldoNeto.TabIndex = 8;
+            // 
+            // txt_ISR
+            // 
+            this.txt_ISR.Location = new System.Drawing.Point(455, 219);
+            this.txt_ISR.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txt_ISR.Name = "txt_ISR";
+            this.txt_ISR.Size = new System.Drawing.Size(116, 20);
+            this.txt_ISR.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(189, 20);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(307, 31);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "CALCULO DE SUELDO";
+            // 
+            // btn_CalcularSueldo
+            // 
+            this.btn_CalcularSueldo.Location = new System.Drawing.Point(486, 85);
+            this.btn_CalcularSueldo.Name = "btn_CalcularSueldo";
+            this.btn_CalcularSueldo.Size = new System.Drawing.Size(85, 23);
+            this.btn_CalcularSueldo.TabIndex = 11;
+            this.btn_CalcularSueldo.Text = "CALCULAR";
+            this.btn_CalcularSueldo.UseVisualStyleBackColor = true;
+            this.btn_CalcularSueldo.Click += new System.EventHandler(this.btn_CalcularSueldo_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(486, 129);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(85, 23);
+            this.btn_limpiar.TabIndex = 12;
+            this.btn_limpiar.Text = "LIMPIAR";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(486, 172);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(85, 23);
+            this.btn_salir.TabIndex = 13;
+            this.btn_salir.Text = "SALIR";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_CalcularSueldo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_ISR);
+            this.Controls.Add(this.txt_SueldoNeto);
+            this.Controls.Add(this.txt_SFS);
+            this.Controls.Add(this.txt_AFP);
+            this.Controls.Add(this.txt_SueldoBruto);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_SueldoBruto;
+        private System.Windows.Forms.TextBox txt_AFP;
+        private System.Windows.Forms.TextBox txt_SFS;
+        private System.Windows.Forms.TextBox txt_SueldoNeto;
+        private System.Windows.Forms.TextBox txt_ISR;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_CalcularSueldo;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
+
---
+++ ISR/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_CalcularSueldo_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txt_SueldoBruto.Text, out double sueldo))
+            {
+                MessageBox.Show("Por favor, ingrese un sueldo válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            double afp = 0.0287;
+            double sfs = 0.0304;
+
+            double AFP = sueldo * afp;
+            double SFS = sueldo * sfs;
+
+            double SueldoNeto = sueldo - AFP - SFS;
+
+            bool ISRaplica = SueldoNeto > 416220.05;
+
+            double ISR = 0.0;
+
+            if (ISRaplica)
+            {
+                ISR = (SueldoNeto - 416220.05) * 0.25;
+            }
+
+            txt_SueldoBruto.Text = SueldoNeto.ToString();
+            txt_AFP.Text = AFP.ToString();
+            txt_SFS.Text = SFS.ToString();
+            txt_ISR.Text = (ISRaplica ? ISR.ToString() : "0.0");
+            txt_SueldoNeto.Text = SueldoNeto.ToString();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_SueldoBruto.Clear();
+            txt_AFP.Clear();
+            txt_SFS.Clear();
+            txt_SueldoNeto.Clear();
+            txt_ISR.Clear();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
